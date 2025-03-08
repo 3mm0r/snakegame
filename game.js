bbcb062be13934bd0aad3c0b85748c723661d5a8
3mm0r <ammarsaid543@gmail.com>
2025-03-08 17:24:05 +0200

    game.js

--- game.js
+++ game.js
@@ -239,7 +239,7 @@ var Snake = (function () {
   return {
     start: function (fps = 15) {
       window.onload = setup;
-      intervalID = setInterval(game.loop,1540 / fps);
+      intervalID = setInterval(game.loop,140 / fps);
     },
 
     loop: game.loop,
